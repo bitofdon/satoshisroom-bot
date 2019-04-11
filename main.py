d6e795fdb9d842c28acdeaedf4de21451e63b083
bitofdon <bitdon@protonmail.com>
2019-04-11 12:54:26 -0500

    connected to CoinMarketCap API, prints our request from the API call

--- main.py
+++ main.py
@@ -1,3 +1,26 @@
 import requests
 import json
 from flask import Flask
+#connect to API token from config.json and pass it
+with open('config.json', 'r') as infile:
+    config = dict(json.load(infile))
+    token = config["token"]
+
+app = Flask(__name__)
+
+#https://pro-api.coinmarketcap.com
+def get_cmc_data(crypto):
+    url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest'
+    params = {'symbol': crypto, 'convert': 'USD'}
+    headers = {'X-CMC_PRO_API_KEY': token}
+
+    r = requests.get(url, headers=headers, params=params).json()
+    print(r)
+
+def main():
+    #print the call of get_cmc_data - BTC as an argument here as example
+    get_cmc_data('BTC') #to get data
+
+if __name__ == '__main__':
+    main()
+    #app.run(debug=True)
